a3f98df3f907ff7d805305d6ea7f03822a0b8c29
Kenneth Okoro <kennethokesiena@gmail.com>
2025-11-12 00:48:24 +0100

    feat(api): added InfrastructureServicesRegistration to DI

--- ABCProperties.Api/Program.cs
+++ ABCProperties.Api/Program.cs
@@ -1,3 +1,4 @@
+using ABCProperties.Infrastructure;
 
 namespace ABCProperties.Api
 {
@@ -13,6 +14,8 @@ namespace ABCProperties.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddInfrastructureServices(builder.Configuration);
 
             var app = builder.Build();
 

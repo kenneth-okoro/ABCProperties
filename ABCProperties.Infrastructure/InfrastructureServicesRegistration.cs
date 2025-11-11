af95519eb537cc614cd04d8ca4de41de7f48ce2a
Kenneth Okoro <kennethokesiena@gmail.com>
2025-11-12 00:42:23 +0100

    feat(infrastructure): add InfrastructureServicesRegistration with DbContext configuration

---
+++ ABCProperties.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ABCProperties.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ABCProperties.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            // Infrastructure service registrations go here
+            return services
+                .AddDbContext<AppDbContext>(options => options
+                    .UseSqlServer(configuration.GetConnectionString("")));
+        }
+    }
+}

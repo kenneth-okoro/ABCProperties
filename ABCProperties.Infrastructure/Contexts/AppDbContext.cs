6a514860e9cf8963315fdd63a26219016dc74b27
Kenneth Okoro <kennethokesiena@gmail.com>
2025-11-12 00:28:52 +0100

    feat(infrastructure): add DbContext

---
+++ ABCProperties.Infrastructure/Contexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ABCProperties.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABCProperties.Infrastructure.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Agent> Agents { get; set; }
+        public DbSet<Property> Properties { get; set; }
+    }
+}

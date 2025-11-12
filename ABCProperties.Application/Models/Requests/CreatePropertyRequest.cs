5e14c3c41c8b07cc8b8a57225c74eab287d8bd18
Kenneth Okoro <kennethokesiena@gmail.com>
2025-11-12 07:16:01 +0100

    feat(application): add Agent and Property request and response models

---
+++ ABCProperties.Application/Models/Requests/CreatePropertyRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ABCProperties.Application.Models.Requests
+{
+    public class CreatePropertyRequest
+    {
+        public int AgentId { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ ABCProperties.Application/Models/Responses/PropertyResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace ABCProperties.Application.Models.Responses
+{
+    public class PropertyResponse
+    {
+        public int Id { get; set; }
+        public int AgentId { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public AgentResponse Agent { get; set; }
+    }
+}

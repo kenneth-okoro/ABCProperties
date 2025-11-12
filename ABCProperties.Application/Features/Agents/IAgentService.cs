bc6ef6bc9bedb78d3ddef012c20518f70456995a
Kenneth Okoro <kennethokesiena@gmail.com>
2025-11-12 07:30:07 +0100

    feat(application): add IAgentService interface with CRUD contract

---
+++ ABCProperties.Application/Features/Agents/IAgentService.cs
@@ -0,0 +1,14 @@
+﻿using ABCProperties.Domain.Entities;
+
+namespace ABCProperties.Application.Features.Agents
+{
+    public interface IAgentService
+    {
+        Task<int> CreateAsync(Agent createAgent);
+        Task<Agent> UpdateAsync(Agent updatedAgent);
+        Task<int> DeleteAsync(int id);
+        Task<Agent> GetByIdAsync(int id);
+        Task<List<Agent>> GetAllAsync();
+        Task<bool> DoesExistAsync(int id);
+    }
+}
